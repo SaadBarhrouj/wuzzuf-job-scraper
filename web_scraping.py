fba17a8e1688efdc3dc1718f6d027b4550c9b961
SaadBarhrouj <saad.barhrouj@etu.uae.ac.ma>
2024-09-01 11:18:12 +0200

    get all the locations names

--- web_scraping.py
+++ web_scraping.py
@@ -21,4 +21,5 @@ soup=BeautifulSoup(src,"lxml")
 #-- job titles|job skills|company|names|location names
 job_titles=soup.find_all("h2",{"class":"css-m604qf"})
 company_names=soup.find_all("a", {"class":"css-17s97q8"})
-print(company_names)
+locations_names=soup.find_all("span",{"class":"css-5wys0k"})
+print(locations_names)
