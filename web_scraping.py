ba341a87bc9f08686d619487be1d8ad6d9394dac
SaadBarhrouj <saad.barhrouj@etu.uae.ac.ma>
2024-09-01 11:11:41 +0200

    get all the company names

--- web_scraping.py
+++ web_scraping.py
@@ -20,4 +20,5 @@ soup=BeautifulSoup(src,"lxml")
 #5th step find the elements containing info we need
 #-- job titles|job skills|company|names|location names
 job_titles=soup.find_all("h2",{"class":"css-m604qf"})
-print(job_titles)
+company_names=soup.find_all("a", {"class":"css-17s97q8"})
+print(company_names)
