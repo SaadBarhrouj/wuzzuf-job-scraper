c6c9725bd2a1cd75f406fd32e96a88fff336341b
SaadBarhrouj <saad.barhrouj@etu.uae.ac.ma>
2024-09-01 13:58:40 +0200

    update

--- web_scraping.py
+++ web_scraping.py
@@ -27,19 +27,22 @@ soup=BeautifulSoup(src,"lxml")
 job_titles=soup.find_all("h2",{"class":"css-m604qf"})
 company_names=soup.find_all("a", {"class":"css-17s97q8"})
 locations_names=soup.find_all("span",{"class":"css-5wys0k"})
+target_divs = soup.find_all("div", {"class": "css-1lh32fc"})
+
 job_skills=[]
-divs = soup.find_all("div")
-
-for div in divs:
-    if div.find("a", {"class": "css-5x9pm1"}):
-        job_skills.append(div)  
-
+for target_div in target_divs:
+    following_div = target_div.find_next_sibling("div") 
+    
+    if following_div:
+        if following_div.find("a", {"class": "css-5x9pm1"}) and following_div.find("a", {"class": "css-o171kl"})  :
+           job_skills.append(following_div)
 #6th step loop over returned lists to extract needed info into other lists
 
 # Iterate over the indices of the 'job_titles' list
 # 'range(len(job_titles))' generates a sequence of numbers from 0 to the length of 'job_titles' - 1
 # For example, if 'job_titles' has 5 elements, 'range(len(job_titles))' will generate 0, 1, 2, 3, 4
 # This allows you to access each element of the list by its index 'i'
+
 for i in range(len(job_titles)):
    job_title.append(job_titles[i].text)
    company_name.append(company_names[i].text)
@@ -47,4 +50,8 @@ for i in range(len(job_titles)):
    skills.append(job_skills[i].text)
 
 
-print(skills)
+
+
+ 
+
+
