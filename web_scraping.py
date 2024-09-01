99aee4edf06439e9db8831259a0dad932fc91c6d
SaadBarhrouj <saad.barhrouj@etu.uae.ac.ma>
2024-09-01 11:41:01 +0200

    get all the job skills

--- web_scraping.py
+++ web_scraping.py
@@ -22,4 +22,12 @@ soup=BeautifulSoup(src,"lxml")
 job_titles=soup.find_all("h2",{"class":"css-m604qf"})
 company_names=soup.find_all("a", {"class":"css-17s97q8"})
 locations_names=soup.find_all("span",{"class":"css-5wys0k"})
-print(locations_names)
+job_skills=[]
+divs = soup.find_all("div")
+
+for div in divs:
+    if div.find("a", {"class": "css-5x9pm1"}):
+        job_skills.append(div)  
+
+
+print(job_skills)
